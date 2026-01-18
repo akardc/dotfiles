603eba4df472570d38da292efe1bbe7e502d00c5
Colin Akard <cakard@onecause.com>
2026-01-18 07:09:52 -0500

    some vim changes

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,11 +1,17 @@
 return {
-  { "nvim-contrib/nvim-ginkgo" },
-  { "nvim-neotest/neotest-jest" },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-contrib/nvim-ginkgo",
+      "nvim-neotest/neotest-jest",
+      "nvim-neotest/neotest-go",
+      "jfpedroza/neotest-elixir",
+    },
     opts = {
       adapters = {
         "nvim-ginkgo",
+        "neotest-elixir",
+        -- "neotest-go",
       },
     },
   },
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      zen = {
+        show = {
+          statusline = true,
+          tabline = true,
+        },
+      },
+      styles = {
+        zen = {
+          backdrop = { transparent = true, blend = 40 },
+          width = 200,
+        },
+      },
+    },
+  },
+}
