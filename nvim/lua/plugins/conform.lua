644db3ba27afe4b69253b4a0a251653f390ac32b
Colin Akard <cakard@onecause.com>
2026-01-29 20:04:48 -0500

    disable sketchybar; update lazyvim a bit

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,6 +2,10 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = true,
+      },
       formatters_by_ft = {
         ["templ"] = { "templ" },
       },
